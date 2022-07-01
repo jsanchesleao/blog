e28c53731dc13a3344b3ea0a91d5559d7460b5e4
Jeferson Sanches <dasilvasanchesle@mediamarktsaturn.com>
2022-07-01 17:39:26 +0200

    fixed install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 script=$(readlink -f $0)
 base=$(dirname $(dirname $script))
@@ -13,7 +13,7 @@ fi
 
 
 if [ ! -f "$toolsScriptPath" ]; then
-echo -e "#!/usr/bin/bash\n
+echo -e "#!/usr/bin/env bash\n
 export BLOG_REPO_DIR=$base 
 export PATH=\$PATH:$base/scripts/cli\n" > $toolsScriptPath
 fi
