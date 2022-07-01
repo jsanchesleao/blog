ce680d76e94d4212c2c29d58b1ad5f55fb290af1
Jeferson Sanches <jsanchesleao@gmail.com>
2022-07-01 15:49:21 +0200

    added scripts

---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+script=$(readlink -f $0)
+base=$(dirname $(dirname $script))
+toolsScriptPath=~/.blogtools.sh
+rcPath=~/.zshrc
+
+grep -q '#BLOG-TOOLS' $rcPath
+if [ $? -eq 0 ]; then
+  echo "Blog tools already installed"
+  exit 1
+fi
+
+
+if [ ! -f "$toolsScriptPath" ]; then
+echo -e "#!/usr/bin/bash\n
+export BLOG_REPO_DIR=$base 
+export PATH=\$PATH:$base/scripts/cli\n" > $toolsScriptPath
+fi
+
+echo -e "\n#BLOG-TOOLS
+source $toolsScriptPath
+" >> $rcPath
---
+++ src/posts/test2.md
@@ -0,0 +1,9 @@
+---
+title: test
+date: 2022-06-30
+draft: true
+---
+
+# This is a test
+
+#### So please do not publish this
